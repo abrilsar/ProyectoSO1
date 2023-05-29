5d2e2f9b226a6b2b76be76d27ed23d6c0bbf22c2
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2023-05-29 09:50:35 -0400

    Class value

---
+++ src/proyectoso1/Values.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoso1;
+
+/**
+ *
+ * @author paola
+ */
+public class Values {
+    static public int[] maxRR;
+    static public int[] maxLG;
+    static public String[] types;
+    static public String[] modos;
+    static public int[] vehicleRR;
+    static public int[] vehicleLG;
+    static public int[] salarys;
+    static public int[] salePriceRR;
+    static public int[] salePriceLG;
+    static public int[] workersQtyRR;
+    static public int[] workersQtyLG;
+
+    public Values() {
+        this.maxRR = new int[6];        
+        this.maxLG = new int[6];    
+        this.types = new String[8];
+        this.modos = new String[4];
+        this.vehicleRR = new int[5];
+        this.vehicleLG = new int[5];
+        this.salarys = new int[8];
+        this.salePriceRR = new int[8];
+    }
+    
+    
+    
+}
