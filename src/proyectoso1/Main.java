af800781a1730c1be49145f8e2c60c81afaf530e
abrilsar <a.sar@correo.unimet.edu.ve>
2023-05-30 18:34:17 -0400

    Read&SaveTxt

--- src/proyectoso1/Main.java
+++ src/proyectoso1/Main.java
@@ -24,14 +24,13 @@ public class Main {
         // TODO code application logic here
         
 //        Txt.leerTxt()
-        initial = new Initial(1, 5);
-        initial.createArray("workersRR", "1", "2", "3", "1", "2", "1");
-        initial.createArray("workersLG", "1", "5", "4", "3", "2", "1");
+        initial = Txt.readData();
 //        Main.form = Global.;
 //        form.setVisible(true);
         Global.startForm(); 
         LGPlant = new VehiclePlant("Lamborghini", Values.maxEmployeesLG, 1000);
-//        RBPlant = new VehiclePlant("Rolls Royce", Values.maxEmployeesRR, 1000);
+        RBPlant = new VehiclePlant("Rolls Royce", Values.maxEmployeesRR, 1000);
+        
     }
     
 }
